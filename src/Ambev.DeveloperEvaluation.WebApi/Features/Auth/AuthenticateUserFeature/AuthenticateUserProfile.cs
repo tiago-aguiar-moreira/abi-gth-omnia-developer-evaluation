50caab2eee803e280ea3b3d442c2d1ed5237ae23
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2025-01-30 21:37:20 -0300

    fix: atualiza mapeamento. Estava gerando erro ao tentar autenticar um usuário.

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
@@ -17,5 +17,7 @@ public sealed class AuthenticateUserProfile : Profile
         CreateMap<User, AuthenticateUserResponse>()
             .ForMember(dest => dest.Token, opt => opt.Ignore())
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
+
+        CreateMap<AuthenticateUserRequest, AuthenticateUserCommand>();
     }
 }
