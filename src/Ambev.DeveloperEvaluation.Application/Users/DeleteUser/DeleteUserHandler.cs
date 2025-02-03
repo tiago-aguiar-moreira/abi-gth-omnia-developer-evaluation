4583193cb6ca8b082cbc4661b70daa2c6dba833a
Tiago Aguiar Moreira <tiago.aguiar1@hotmail.com>
2025-02-03 01:10:21 -0300

    chore: remove classe desnecessária e comentário errado

--- src/Ambev.DeveloperEvaluation.Application/Users/DeleteUser/DeleteUserHandler.cs
+++ src/Ambev.DeveloperEvaluation.Application/Users/DeleteUser/DeleteUserHandler.cs
@@ -15,9 +15,7 @@ public class DeleteUserHandler : IRequestHandler<DeleteUserCommand, DeleteUserRe
     /// Initializes a new instance of DeleteUserHandler
     /// </summary>
     /// <param name="userRepository">The user repository</param>
-    /// <param name="validator">The validator for DeleteUserCommand</param>
-    public DeleteUserHandler(
-        IUserRepository userRepository)
+    public DeleteUserHandler(IUserRepository userRepository)
     {
         _userRepository = userRepository;
     }
